53dcf307d6a4555fb2caa62145fdc4bcc7e397a7
sunjid-git <sunjid.info@gmail.com>
2021-09-30 22:42:26 +0600

    showing players

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,39 +1,44 @@
 import React, { useState } from "react";
+import './Home.css';
+
 import Header from "../Header/Header";
 import playerData from "../data.json";
 import PlayersList from "../PlayersList/PlayersList";
 
 function Home() {
+
   const [loading, setLoading] = useState(true);
   const [players, setPlayers] = useState(playerData);
   const [category, setCategory] = useState("cricket");
-
   const [search, setSearch] = useState("");
 
   const matchedCategory = playerData.filter(
     (data) => data.category === category
   );
+//   console.log(matchedCategory);
 
   const searchedResult = matchedCategory.filter((player) =>
     player.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
   );
 
-  console.log("matchedCategory", matchedCategory);
+//   console.log("matchedCategory", matchedCategory);
 
   const getSpecificPlayers = (e) => {
     setSearch(e.target.value);
+    // console.log(e.target.value);
   };
+
 
   const getPLayerDetails = matchedCategory.filter((player) =>
     player.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
   );
+//   console.log("getPLayer", getPLayerDetails);
 
-  console.log("getPLayer", getPLayerDetails);
   return (
     <>
       <Header setCategory={setCategory}></Header>
 
-      <div className="show-players">
+      <div className="show-all-players">
         <div className="player-box-left">
           <div className="search-box">
             <input
@@ -52,7 +57,7 @@ function Home() {
 
         </div>
 
-        <div className="player-box-right"></div>
+        <div className="player-box-right">mafi</div>
       </div>
     </>
   );
--- src/components/PlayersList/PlayersList.js
+++ src/components/PlayersList/PlayersList.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 function PlayersList(props) {
 
-    const {name, img, email, hirePrice, currentClub, category} = props.player;
+    const {name, img, email, hirePrice, currentClub} = props.player;
     return (
-        <div>
+        <div className="single-player">
+            <img src={img} alt="Player Name" />
             <h1>{name}</h1>
+            <p>Hiring Price: <strong> ${email}</strong> </p>
+            <p>Hiring Price: <strong> ${hirePrice}</strong> </p>
+            <p>Current Club: <strong> ${currentClub}</strong> </p>
         </div>
     )
 }
