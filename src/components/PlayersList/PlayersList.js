d8d2e07f98d0ff16e691321367cb4fe6ae96b3cf
sunjid-git <sunjid.info@gmail.com>
2021-10-01 11:47:48 +0600

    Sports Mania

--- src/components/PlayersList/PlayersList.js
+++ src/components/PlayersList/PlayersList.js
@@ -7,14 +7,15 @@ function PlayersList(props) {
     <div className="single-player">
       <img src={img} alt="Player Name" />
       <p>{name}</p>
-      <p>
-      Email <strong> ${email}</strong>{" "}
-      </p>
+     
       <p>
         Hiring Price: <strong> ${hirePrice}</strong>{" "}
       </p>
       <p>
-        Club: <strong> ${currentClub}</strong>{" "}
+        Club: <strong> {currentClub}</strong>{" "}
+      </p>
+      <p>
+      Email: <strong> {email}</strong>{" "}
       </p>
 
       <button onClick={()=> props.addPlayer(props.player)} className="sports-button">
